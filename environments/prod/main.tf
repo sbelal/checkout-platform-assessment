d345ef22e6fabfc20f1724de58803467080bf121
Sal Belal <sbelal@gmail.com>
2026-03-18 13:57:09 +0000

    Performed Composition Module refactor

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,12 +1,6 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-checkout-assessment-${var.environment}"
-  location = var.location
-}
-
-module "vnet" {
-  source              = "../../modules/vnet"
-  vnet_name           = "vnet-checkout-assessment-${var.environment}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  address_space       = var.vnet_address_space
+module "infrastructure" {
+  source             = "../../modules/infrastructure"
+  environment        = var.environment
+  location           = var.location
+  vnet_address_space = var.vnet_address_space
 }
